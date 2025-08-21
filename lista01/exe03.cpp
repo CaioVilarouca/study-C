540a170c2862473f90bf4bb1bbc8aa64d37e1584
CaioVilarouca <caiovilarouca@gmail.com>
2025-08-21 20:42:17 -0300

    exe03 Lista01

---
+++ lista01/exe03.cpp
@@ -0,0 +1,12 @@
+//
+// Created by caio on 21/08/2025.
+//
+#include <cstdio>
+#include "exe03.h"
+
+int main() {
+    printf("informe um numero: ");
+    int number;
+    scanf("%d", &number);
+    printf("Foi informado o valor %d", number);
+}
