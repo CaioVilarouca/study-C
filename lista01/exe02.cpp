2117851e19cefde27eb1b76f64a059d544d82767
CaioVilarouca <caiovilarouca@gmail.com>
2025-08-21 20:37:00 -0300

    exe02 da list  01

---
+++ lista01/exe02.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//
+// Created by caio on 21/08/2025.
+//
+int main() {
+    printf("Informe um numero: ");
+    int number;
+    scanf("%d", &number);
+    printf("O numero digitado %d", number);
+}
---
+++ lista01/exe02.h
@@ -0,0 +1,14 @@
+//
+// Created by caio on 21/08/2025.
+//
+
+#ifndef PROJETC_C_EXE02_H
+#define PROJETC_C_EXE02_H
+
+
+class exe02 {
+
+};
+
+
+#endif //PROJETC_C_EXE02_H
