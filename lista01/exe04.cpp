01105b37cb41e0f440e738eddd20a73693809bd3
CaioVilarouca <caiovilarouca@gmail.com>
2025-08-21 20:48:21 -0300

    oxe04 da lista01

---
+++ lista01/exe04.cpp
@@ -0,0 +1,14 @@
+//
+// Created by caio on 21/08/2025.
+//
+#include <cstdio>
+#include "exe04.h"
+
+int main() {
+    int number01, number02;
+    printf("Informe um numero inteiro: ");
+    scanf("%d", &number01);
+    printf("Informe o segundo numero inteiro: ");
+    scanf("%d", &number02);
+    printf("Voce informou os numeros %d e %i:", number01, number02);
+}
