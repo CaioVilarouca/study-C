64eb7d609acf8cfe2789baca0e4f8d366485d105
CaioVilarouca <caiovilarouca@gmail.com>
2025-08-21 20:51:47 -0300

    exe05 lista01

---
+++ lista01/exe05.cpp
@@ -0,0 +1,13 @@
+//
+// Created by caio on 21/08/2025.
+//
+
+#include "exe05.h"
+#include <cstdio>
+
+int main() {
+    float number;
+    printf("Informe um numero float :");
+    scanf("%f", &number);
+    printf("Voce informou o numero %.2f", number);
+}
---
+++ lista01/exe05.h
@@ -0,0 +1,13 @@
+//
+// Created by caio on 21/08/2025.
+//
+
+#ifndef PROJETC_C_EXE05_H
+#define PROJETC_C_EXE05_H
+
+
+class exe05 {
+};
+
+
+#endif //PROJETC_C_EXE05_H
