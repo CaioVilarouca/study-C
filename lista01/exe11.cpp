ae1fbddc1c2b300b01afeb8cbc0edbb32ff2278f
CaioVilarouca <caiovilarouca@gmail.com>
2025-08-21 21:29:53 -0300

    exe11 lista01

---
+++ lista01/exe11.cpp
@@ -0,0 +1,12 @@
+//
+// Created by caio on 21/08/2025.
+//
+
+#include "exe11.h"
+#include <cstdio>
+int main() {
+    float number;
+    printf("Informe um numero: ");
+    scanf("%f", &number);
+    printf("Voce informou o numero %.2f e o dobro dele %.2f", number, number * 2);
+}
---
+++ lista01/exe11.h
@@ -0,0 +1,13 @@
+//
+// Created by caio on 21/08/2025.
+//
+
+#ifndef PROJETC_C_EXE11_H
+#define PROJETC_C_EXE11_H
+
+
+class exe11 {
+};
+
+
+#endif //PROJETC_C_EXE11_H
