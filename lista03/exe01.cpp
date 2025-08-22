3f79ceda8cc7d4cf76f3a4da480570dc4cf552a6
CaioVilarouca <caiovilarouca@gmail.com>
2025-08-22 15:13:59 -0300

    exe01,02 e 03 lista03

---
+++ lista03/exe01.cpp
@@ -0,0 +1,11 @@
+//
+// Created by caio on 22/08/2025.
+//
+
+#include "exe01.h"
+#include <cstdio>
+int main() {
+    for (int i = 0; i < 10; i++) {
+        printf("Hello Word %i \n", i + 1);
+    }
+}
---
+++ lista03/exe01.h
@@ -0,0 +1,13 @@
+//
+// Created by caio on 22/08/2025.
+//
+
+#ifndef PROJETC_C_EXE01_H
+#define PROJETC_C_EXE01_H
+
+
+class exe01 {
+};
+
+
+#endif //PROJETC_C_EXE01_H
