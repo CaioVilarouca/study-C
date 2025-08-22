c610a11cd444631d7078abfbd85232ee68dffa50
CaioVilarouca <caiovilarouca@gmail.com>
2025-08-21 21:13:02 -0300

    exe08 lista01

---
+++ lista01/exe08.cpp
@@ -0,0 +1,12 @@
+//
+// Created by caio on 21/08/2025.
+//
+
+#include "exe08.h"
+#include <cstdio>
+int main() {
+    char sexy;
+    printf("Informe seu sexo M/F: ");
+    scanf(" %c", &sexy);
+    printf("Seu sexo e %c", sexy);
+} 
---
+++ lista01/exe08.h
@@ -0,0 +1,13 @@
+//
+// Created by caio on 21/08/2025.
+//
+
+#ifndef PROJETC_C_EXE08_H
+#define PROJETC_C_EXE08_H
+
+
+class exe08 {
+};
+
+
+#endif //PROJETC_C_EXE08_H
