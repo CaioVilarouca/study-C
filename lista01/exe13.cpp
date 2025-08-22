e66bfdf7738f7b7a787167a6c5106a0caf7d16db
CaioVilarouca <caiovilarouca@gmail.com>
2025-08-21 21:39:48 -0300

    exe13 lista01

---
+++ lista01/exe13.cpp
@@ -0,0 +1,14 @@
+//
+// Created by caio on 21/08/2025.
+//
+
+#include "exe13.h"
+#include <cstdio>
+int main () {
+    int number01, number02;
+    printf("Informe um numero: ");
+    scanf("%i", &number01);
+    printf("Informe um numero: ");
+    scanf("%i", &number02);
+    printf("O numero %i e %i somado: %i", number01, number02, number01 + number02);
+}
---
+++ lista01/exe13.h
@@ -0,0 +1,13 @@
+//
+// Created by caio on 21/08/2025.
+//
+
+#ifndef PROJETC_C_EXE13_H
+#define PROJETC_C_EXE13_H
+
+
+class exe13 {
+};
+
+
+#endif //PROJETC_C_EXE13_H
