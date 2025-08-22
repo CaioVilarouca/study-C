ec0574789cdd16b5e937bc66fe065c3557f4abf1
CaioVilarouca <caiovilarouca@gmail.com>
2025-08-22 15:30:17 -0300

    exe08 lista03

---
+++ lista03/exe08.cpp
@@ -0,0 +1,13 @@
+//
+// Created by caio on 22/08/2025.
+//
+
+#include "exe08.h"
+#include <cstdio>
+int main() {
+    int i, value = 0;
+    for (i=1; i<=100; i++) {
+        value =+ i;
+        printf("soma %d \n", value);
+    }
+}
